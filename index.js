f2734992884381bf85bd1c80f735a543e826a055
Mohammed Al-Rozzi <irozzi@icloud.com>
2023-12-20 13:08:49 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+// Sample data structure (replace this with your actual data structure)
+let incidents = [
+  {
+    tokenNumber: 1,
+    connectedTo: "Some connection",
+    dateOfIncident: "2023-12-20",
+    dateOfReporting: "2023-12-21",
+    geolocation: "latitude,longitude",
+    area: "Some Area",
+    reportedBy: "John Doe",
+  },
+  // Add more incidents as needed
+];
+
+app.get('/', (req, res) => {
+  // Respond with the organized data
+  const organizedData = incidents.map((incident) => ({
+    tokenNumber: incident.tokenNumber,
+    connectedTo: incident.connectedTo,
+    dateOfIncident: incident.dateOfIncident,
+    dateOfReporting: incident.dateOfReporting,
+    geolocation: incident.geolocation,
+    area: incident.area,
+    reportedBy: incident.reportedBy,
+  }));
+
+  res.json(organizedData);
+});
+
+// Add a new incident
+app.post('/add-incident', (req, res) => {
+  const newIncident = req.body;
+  incidents.push(newIncident);
+  res.json({ message: 'Incident added successfully', newIncident });
+});
+
+// Retrieve JSON data for a specific tokenNumber
+app.get('/incident/:tokenNumber', (req, res) => {
+  const tokenNumber = parseInt(req.params.tokenNumber, 10);
+  const incident = incidents.find((item) => item.tokenNumber === tokenNumber);
+
+  if (incident) {
+    res.json(incident);
+  } else {
+    res.status(404).json({ message: 'Incident not found' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
