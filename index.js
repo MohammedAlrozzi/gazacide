15b0ae85895969f0c703d2a095beb23d8ec95551
Mohammed Al-Rozzi <irozzi@icloud.com>
2023-12-20 14:36:59 +0000

    commit 8

--- index.js
+++ index.js
@@ -1,61 +1,3 @@
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const app = express();
-// const PORT = process.env.PORT || 3000;
-
-// app.use(bodyParser.json());
-
-// // Sample data structure (replace this with your actual data structure)
-// let incidents = [
-//   {
-//     tokenNumber: 1,
-//     connectedTo: "Some connection",
-//     dateOfIncident: "2023-12-20",
-//     dateOfReporting: "2023-12-21",
-//     geolocation: "latitude,longitude",
-//     area: "Some Area",
-//     reportedBy: "John Doe",
-//   },
-//   // Add more incidents as needed
-// ];
-
-// app.get('/', (req, res) => {
-//   // Respond with the organized data
-//   const organizedData = incidents.map((incident) => ({
-//     tokenNumber: incident.tokenNumber,
-//     connectedTo: incident.connectedTo,
-//     dateOfIncident: incident.dateOfIncident,
-//     dateOfReporting: incident.dateOfReporting,
-//     geolocation: incident.geolocation,
-//     area: incident.area,
-//     reportedBy: incident.reportedBy,
-//   }));
-
-//   res.json(organizedData);
-// });
-
-// // Add a new incident
-// app.post('/add-incident', (req, res) => {
-//   const newIncident = req.body;
-//   incidents.push(newIncident);
-//   res.json({ message: 'Incident added successfully', newIncident });
-// });
-
-// // Retrieve JSON data for a specific tokenNumber
-// app.get('/incident/:tokenNumber', (req, res) => {
-//   const tokenNumber = parseInt(req.params.tokenNumber, 10);
-//   const incident = incidents.find((item) => item.tokenNumber === tokenNumber);
-
-//   if (incident) {
-//     res.json(incident);
-//   } else {
-//     res.status(404).json({ message: 'Incident not found' });
-//   }
-// });
-
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
@@ -72,6 +14,8 @@ let incidents = [];
 fs.createReadStream('data.csv')
   .pipe(csv())
   .on('data', (row) => {
+    // Add a tokenNumber to each row
+    row.tokenNumber = incidents.length + 1;
     incidents.push(row);
   })
   .on('end', () => {
@@ -85,7 +29,9 @@ app.get('/', (req, res) => {
     connectedTo: incident.connectedTo,
     dateOfIncident: incident.dateOfIncident,
     dateOfReporting: incident.dateOfReporting,
-    geolocation: incident.geolocation,
+    geolocationLat: incident.geolocationLat,
+    geolocationLong: incident.geolocationLong,
+
     area: incident.area,
     reportedBy: incident.reportedBy,
   }));
@@ -96,6 +42,7 @@ app.get('/', (req, res) => {
 // Add a new incident
 app.post('/add-incident', (req, res) => {
   const newIncident = req.body;
+  newIncident.tokenNumber = incidents.length + 1;
   incidents.push(newIncident);
   res.json({ message: 'Incident added successfully', newIncident });
 });
@@ -103,10 +50,7 @@ app.post('/add-incident', (req, res) => {
 // Retrieve JSON data for a specific tokenNumber
 app.get('/incident/:tokenNumber', (req, res) => {
   const tokenNumber = parseInt(req.params.tokenNumber, 10);
-  const incident = incidents.find((item) => item.tokenNumber === tokenNumber.toString());
-
-
-
+  const incident = incidents.find((item) => item.tokenNumber === tokenNumber);
 
   if (incident) {
     res.json(incident);
